f8b01df0c3cacd753c1df2e80228051463957a0a
ajoy45 <ajoypust@gmail.com>
2023-04-06 16:59:30 +0600

    books page complete

---
+++ src/components/Book.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Book = ({ book }) => {
+    console.log(book)
+    const { title, image, price, isbn13, subtitle, url } = book
+    return (
+        <div className=''>
+            <div className="card card-side bg-purple-300 shadow-xl relative">
+                <figure><img src={image} alt="book" /></figure>
+                <div className="card-body ">
+                    <h2 className="card-title">{title}</h2>
+                    <p>{subtitle}</p>
+                    <p>price:{price}</p>
+
+                </div>
+                <div className='bg-black bg-opacity-40 text-white  opacity-0 hover:opacity-100  absolute inset-0 flex justify-center items-center text-center rounded-2xl'>
+                        <div>
+                            <h1>{title}</h1>
+                            <p>{price}</p>
+                            <p>{url }</p>
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Book;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Book from './Book';
 
 const Books = () => {
+    const data=useLoaderData();
+    const books=data.books
     return (
-        <div>
-            <h1>this is book page</h1>
+        <div className='px-24 grid grid-cols-4 gap-8 my-5'>
+             {
+                books.map(book=><Book
+                key={book.isbn13}
+                 book={book}
+                ></Book>)
+             }
         </div>
     );
 };
